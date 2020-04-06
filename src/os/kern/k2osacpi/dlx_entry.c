51c0696aa09bd7200be2fe7795079159c4c6f323
Kurt Kennett <kkennett@ntdev.microsoft.com>
2020-04-06 09:18:53 -0700

    add copyright headers, set up power button handler

--- src/os/kern/k2osacpi/dlx_entry.c
+++ src/os/kern/k2osacpi/dlx_entry.c
@@ -1,3 +1,35 @@
+//   
+//   BSD 3-Clause License
+//   
+//   Copyright (c) 2020, Kurt Kennett
+//   All rights reserved.
+//   
+//   Redistribution and use in source and binary forms, with or without
+//   modification, are permitted provided that the following conditions are met:
+//   
+//   1. Redistributions of source code must retain the above copyright notice, this
+//      list of conditions and the following disclaimer.
+//   
+//   2. Redistributions in binary form must reproduce the above copyright notice,
+//      this list of conditions and the following disclaimer in the documentation
+//      and/or other materials provided with the distribution.
+//   
+//   3. Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//   
+//   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+//   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
 #include "k2osacpi.h"
 
 static void
@@ -40,6 +72,15 @@ RegionHandler(
     return (AE_OK);
 }
 
+static UINT32
+PowerButtonHandler(
+    void *Context
+)
+{
+    K2OSKERN_Debug("K2OSACPI:PowerButtonHandler\n");
+    return (AE_OK);
+}
+
 static ACPI_STATUS
 sInstallHandlers(void)
 {
@@ -61,22 +102,29 @@ sInstallHandlers(void)
         return (Status);
     }
 
+    Status = AcpiInstallFixedEventHandler(ACPI_EVENT_POWER_BUTTON, PowerButtonHandler, NULL);
+    if (ACPI_FAILURE(Status))
+    {
+        K2OSKERN_Debug("K2OSACPI: Failure will installing Power Button handler\n");
+        return (Status);
+    }
+
     return (AE_OK);
 }
 
 #define CHK_WATER 0
 
-K2STAT 
-K2_CALLCONV_REGS 
+K2STAT
+K2_CALLCONV_REGS
 dlx_entry(
     DLX *   apDlx,
     UINT32  aReason
-    )
+)
 {
-    ACPI_STATUS     Status;
+    ACPI_STATUS         Status;
 #if CHK_WATER
-    K2LIST_LINK *   pListLink;
-    CACHE_HDR *     pCache;
+    K2LIST_LINK *       pListLink;
+    CACHE_HDR *         pCache;
 #endif
 
     Status = AcpiInitializeSubsystem();
