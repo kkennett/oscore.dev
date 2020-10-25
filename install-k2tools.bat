e69b3d7cbba379b1342a9e1a0a28ed9882b00282
Kurt Kennett <kurt_kennett@hotmail.com>
2020-10-25 12:17:14 -0700

    update to v2 of k2tools32

--- install-k2tools.bat
+++ install-k2tools.bat
@@ -34,7 +34,7 @@ echo.
 echo Installing k2tools via nuget
 echo.
 type nul
-nuget.exe install k2tools32 -Version 1.0.0 -Source https://www.myget.org/F/k2tools32/api/v2 > NUL
+nuget.exe install k2tools32 -Version 2.0.0 -Source https://www.myget.org/F/k2tools32/api/v2 > NUL
 if "%errorlevel%"=="0" goto continue1
 echo.
 echo Error %errorlevel% calling nuget.  Is it installed and on path?
@@ -42,7 +42,7 @@ echo.
 goto :EOF
 :continue1
 if exist k2tools32\. rd /s /q k2tools32
-move k2tools32.1.0.0\k2tools32 . > NUL
-rd /s /q k2tools32.1.0.0
+move k2tools32.2.0.0\k2tools32 . > NUL
+rd /s /q k2tools32.2.0.0
 echo.
 echo Tools installed. You can run k2tools32\k2setup now
