1810d7dfe9d6d10a5b6fc06433db0934b4215f33
Kurt Kennett <kurt_kennett@hotmail.com>
2020-11-21 10:56:34 -0800

    fix disk target for kernel and image file; fix update batch file

--- src/demo/virtpc/u.bat
+++ src/demo/virtpc/u.bat
@@ -7,5 +7,7 @@ goto :DoneCheck
 set _K2_VHD_DRIVE=D
 :DoneCheck
 call attach c:\repo\oscore.dev\vhd\x86disk.vhd
+rd /s /q %_K2_VHD_DRIVE%:\efi
+rd /s /q %_K2_VHD_DRIVE%:\k2os
 xcopy /Y c:\repo\oscore.dev\img\x32\debug\demo\virtpc\bootdisk\*.* %_K2_VHD_DRIVE%:\ /e /s
 call detach c:\repo\oscore.dev\vhd\x86disk.vhd
