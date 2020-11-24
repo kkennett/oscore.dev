f00cd837963cde794205a2f3a0c1fea745c20931
Kurt Kennett <kkennett@ntdev.microsoft.com>
2020-11-23 16:31:27 -0800

    normalise u.bat

--- src/demo/virtpc/u.bat
+++ src/demo/virtpc/u.bat
@@ -1,13 +1,8 @@
 @echo off
-if /I "%COMPUTERNAME%" == "ZEFPHYR" goto :IsZefphyr
-:NotRaven
-set _K2_VHD_DRIVE=H
-goto :DoneCheck
-:IsZefphyr
-set _K2_VHD_DRIVE=D
-:DoneCheck
+if /i "%1"=="" goto :end
 call attach c:\repo\oscore.dev\vhd\x86disk.vhd
-rd /s /q %_K2_VHD_DRIVE%:\efi
-rd /s /q %_K2_VHD_DRIVE%:\k2os
-xcopy /Y c:\repo\oscore.dev\img\x32\debug\demo\virtpc\bootdisk\*.* %_K2_VHD_DRIVE%:\ /e /s
+rd /s /q %1%\efi
+rd /s /q %1%\k2os
+xcopy /Y c:\repo\oscore.dev\img\x32\debug\demo\virtpc\bootdisk\*.* %1%\ /e /s
 call detach c:\repo\oscore.dev\vhd\x86disk.vhd
+:end
