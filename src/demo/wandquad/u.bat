13ed395a9c9a91c0aa1829a4beb7c66bcff970e9
Kurt Kennett <kurt_kennett@hotmail.com>
2021-01-10 09:01:03 -0800

    target wand appropriately.

--- src/demo/wandquad/u.bat
+++ src/demo/wandquad/u.bat
@@ -2,5 +2,5 @@
 if /i "%1"=="" goto :end
 rd /s /q %1%\efi
 rd /s /q %1%\k2os
-xcopy /Y c:\repo\oscore.dev\img\a32\debug\demo\udooquad\bootdisk\*.* %1%\ /e /s
+xcopy /Y c:\repo\oscore.dev\img\a32\debug\demo\wandquad\bootdisk\*.* %1%\ /e /s
 :end
